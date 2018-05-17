21386de892a0ac12d7637c40f192fed415e7aa5a
Joseph <jcastelan02@gmail.com>
2018-05-16 19:29:32 -0700

    population density map seems to look fine

--- canadaPopulationDensity.js
+++ canadaPopulationDensity.js
@@ -26,34 +26,48 @@ var svg = d3.select("body")
             .attr("height", h)
             .attr("text-align", "center");
 
-//Load in agriculture data
-d3.csv("canadaPopulationDensity.csv", function(data) {
+//Define Tooltip here
+var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
 
+//Load in agriculture data
+d3.csv("canadaPopulation2016simple.csv", function(error, data) {
+
+    if(error) throw error;
+    data.forEach( function(d){
+        d.value = +d.value;
+        d.area = +d.area; //in km squared
+        d.popDensity = d.value/d.area;
+        console.log(d);
+    })
+    
+    console.log(d3.extent( data, function(d) { return d.popDensity;}));
     //Set input domain for color scale
-    color.domain([
-        d3.min(data, function(d) { return d.value; }), 
-        d3.max(data, function(d) { return d.value; })
-    ]);
+    color.domain(d3.extent( data, function(d) { return d.popDensity;})
+        /*[
+        d3.min(data, function(d) { return d.population; }), 
+        d3.max(data, function(d) { return d.population; })
+    ]*/);
 
     //Load in GeoJSON data
     d3.json("canada.json", function(json) {
 
-        //Merge the ag. data and GeoJSON
-        //Loop through once for each ag. data value
+        //Merge the data and GeoJSON
+        //Loop through once for each data value
         for (var i = 0; i < data.length; i++) {
-
-            var dataState = data[i].state;	
+            var dataLoc = data[i].location;	
             var dataValue = parseFloat(data[i].value);	//Grab data value, and convert from string to float
-
+            var dataArea = parseFloat(data[i].area);
+            var dataPopDensity = parseFloat(data[i].popDensity);
+            
             //Find the corresponding state inside the GeoJSON
             for (var j = 0; j < json.features.length; j++) {
 
-                var jsonState = json.features[j].properties.name;
-
-                if (dataState == jsonState) {
-
+                var jsonLoc = json.features[j].properties.Name;
+                if (dataLoc == jsonLoc) {
                     //Copy the data value into the JSON
-                    json.features[j].properties.value = dataValue;
+                    json.features[j].properties.value = dataPopDensity;
 
                     //Stop looking through the JSON
                     break;
@@ -77,12 +91,23 @@ d3.csv("canadaPopulationDensity.csv", function(data) {
                     return color(value);
                 } else {
                     //If value is undefined…
-                    return "#ccc";
+                    return "black";
                 }
-           });
-
-
-
+           }).on("mouseover", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .95);
+            div.html( "<b>"+d.properties.Name+
+                     "</br>Population Density: "+ d.properties.value+"</b>")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px");
+       })
+       .on("mouseout", function(d) {
+            div.transition()
+               .duration(500)
+               .style("opacity", 0);
+       });
+        
 
     });
 
